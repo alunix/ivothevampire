93cc883b631a5b0abf7a027a4040ac43c1add3c4
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-09-07 21:48:35 +0200

    Code refactoring

--- src/vampire_ivo.cpp
+++ src/vampire_ivo.cpp
@@ -1,5 +1,28 @@
-// vampire_ivo.cpp : Defines the entry point for the console application.
-//
+/*
+
+The MIT License
+
+Copyright (c) 2005 Petar Petrov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 
 /*
 
@@ -11,21 +34,22 @@
   beta release: 21.03.2005
   final release: ??.03.2005
 
-  code: Peter Petrov
+  code: Petar Petrov
   gfx & sfx: Valentin Bojilov
 
 */
 
 
-#include "stdafx.h"
+#include "vampire_ivo.h"
 #include "GameProc.h"
 
 
 int main(int argc, char* argv[])
 {
 
-	if ( !VampireIvo::InitGame( 800, 600, 16, true ) )
+	if (!VampireIvo::InitGame(800, 600, 16, true)) {
 		return -1;
+	}
 
 	VampireIvo::StartGameLoop();
 	
